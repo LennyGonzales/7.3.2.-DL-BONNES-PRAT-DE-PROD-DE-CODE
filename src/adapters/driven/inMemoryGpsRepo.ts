e0d68a1e0b30faa12261c1cddb1961de9383583b
LennyGonzales <lenny.gonzales.page@gmail.com>
2026-01-13 11:42:52 +0100

    fix: update in memory repo

--- src/adapters/driven/inMemoryGpsRepo.ts
+++ src/adapters/driven/inMemoryGpsRepo.ts
@@ -2,21 +2,21 @@ import { v4 as uuidv4 } from 'uuid';
 import { Gps } from '../../domain/gps';
 import { GpsRepositoryPort } from '../../ports/driven/gpsRepositoryPort';
 
-const store: Gps[] = [];
-
 export class inMemoryGpsRepo implements GpsRepositoryPort {
+  constructor(private readonly store: Gps[] = []) {}
+
   async findAllByUserId(user_id: string): Promise<Gps[]> {
-    return store.filter((s) => s.user_id === user_id);
+    return this.store.filter((s) => s.user_id === user_id);
   }
 
   async findById(id: string): Promise<Gps | null> {
-    const found = store.find((s) => s.id === id);
+    const found = this.store.find((s) => s.id === id);
     return found ?? null;
   }
 
   async save(gps: Omit<Gps, 'id'>): Promise<Gps> {
     const newGps: Gps = new Gps(gps.user_id, gps.timestamp, gps.latitude, gps.longitude, uuidv4());
-    store.push(newGps);
+    this.store.push(newGps);
     return newGps;
   }
 }
